dbb9907290de31e021d4c2551159ad04994cb964
okoch <okoch@terrania.universum>
2019-07-07 15:11:43 +0200

    new x86

--- scripts/routerAndMiracast.sh
+++ scripts/routerAndMiracast.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-sudo apt-get install check
-sudo apt-get install libgstreamer1.0-0
-sudo apt-get install gstreamer1.0-tools
-sudo apt-get install build-essential
-sudo apt-get install ubuntu-restricted-extras
-sudo apt-get install pkg-config
-sudo apt-get install libglib2.0-dev 
-sudo apt-get install libreadline-dev
-sudo apt-get install libudev-dev
-sudo apt-get install libsystemd-dev
-sudo apt-get install libusb-dev
-sudo apt-get install cmake 
+sudo apt-get install -y check
+sudo apt-get install -y libgstreamer1.0-0
+sudo apt-get install -y gstreamer1.0-tools
+sudo apt-get install -y build-essential
+sudo apt-get install -y ubuntu-restricted-extras
+sudo apt-get install -y pkg-config
+sudo apt-get install -y libglib2.0-dev 
+sudo apt-get install -y libreadline-dev
+sudo apt-get install -y libudev-dev
+sudo apt-get install -y libsystemd-dev
+sudo apt-get install -y libusb-dev
+sudo apt-get install -y cmake 
 cp ./miraclecast/ res/org.freedesktop.miracle.conf to /etc/dbus-1/system.d/
 cd ./miracalecast
 mkdir build
