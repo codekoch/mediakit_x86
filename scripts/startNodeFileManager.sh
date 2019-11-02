56913bb4e7401f0361a14be82b66c129782a384b
okoch <okoch@terrania.universum>
2019-11-02 14:05:59 +0100

    many changes

--- scripts/startNodeFileManager.sh
+++ scripts/startNodeFileManager.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IP=`/sbin/ifconfig | grep -i "inet " | awk '{print $2}' | head -1`
+IP=`/sbin/ip route get 1 | awk '{print $NF;exit}' | head -1`
 qrencode -s 10 -o serverQRCode.png "http://$IP:8080"
 mkdir ~/Uploads
 thunar ~/Uploads &
