93473416f81a90d9127b7d7906f42a159936624a
okoch <okoch@terrania.universum>
2019-11-02 15:54:11 +0100

    many changes

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -32,7 +32,7 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y feh
 sudo apt-get install -y gparted
 sudo apt-get install -y net-tools
-
+sudo apt-get install -y vlc
 #### Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
